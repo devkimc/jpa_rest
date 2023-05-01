1f46b2cb441296678e52db17a348f9561c1e5e41
WooSeok Kim <kws60000@naver.com>
2023-05-01 15:35:18 +0900

    feat: 쿠키 유틸 메서드 추가

---
+++ src/main/java/jparest/practice/common/util/CookieUtils.java
@@ -0,0 +1,56 @@
+package jparest.practice.common.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.util.SerializationUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
+import java.util.Optional;
+
+@UtilityClass
+public class CookieUtils {
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public static void addCookie(HttpServletResponse response, String name, String value, String domain) {
+        Cookie cookie = new Cookie(name, serialize(value));
+        cookie.setDomain(domain);
+        cookie.setPath("/");
+        cookie.setMaxAge((360000));
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+    }
+
+    // 쿠키 직렬화
+    private static String serialize(Object obj) {
+        return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(obj));
+    }
+}
