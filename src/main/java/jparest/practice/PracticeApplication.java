ceb8395f37056e351a4d770d4296fb104b6c1ba8
WooSeok Kim <kws60000@naver.com>
2023-05-02 23:11:22 +0900

    feat: CorsConfig 설정

--- src/main/java/jparest/practice/PracticeApplication.java
+++ src/main/java/jparest/practice/PracticeApplication.java
@@ -1,9 +1,14 @@
 package jparest.practice;
 
+import jparest.practice.auth.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		CorsConfig.class
+})
 public class PracticeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/jparest/practice/auth/config/SecurityConfig.java
+++ src/main/java/jparest/practice/auth/config/SecurityConfig.java
@@ -14,11 +14,18 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig  {
+    private final CorsConfig corsProperties;
     private final JwtFilter jwtFilter;
 
     // JWT 제공 클래스
@@ -60,6 +67,21 @@ public class SecurityConfig  {
                 .accessDeniedHandler(accessDeniedHandler) // 인가 실패
                 .and()
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource corsConfigSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
+        corsConfig.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
+        corsConfig.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setMaxAge(corsConfig.getMaxAge());
+
+        corsConfigSource.registerCorsConfiguration("/**", corsConfig);
+        return corsConfigSource;
     }
 
     /**
