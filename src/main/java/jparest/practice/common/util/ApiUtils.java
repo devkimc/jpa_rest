4ab07dde70f1ef51ac97406c2774188dbc332daa
WooSeok Kim <kws60000@naver.com>
2023-05-07 14:27:13 +0900

    feat: responseType 유틸클래스 추가

---
+++ src/main/java/jparest/practice/common/util/ApiUtils.java
@@ -0,0 +1,15 @@
+package jparest.practice.common.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApiUtils {
+
+    public static <T> ApiResult<T> success(T result) {
+        return new ApiResult<>(true, result);
+    }
+
+    public static <T> ApiResult<T> fail(T result) {
+        return new ApiResult<>(false, result);
+    }
+}
