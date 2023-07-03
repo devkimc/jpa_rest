c7a7a2656c7ee5a31a41fa391658232701a1fec9
WooSeok Kim <kws60000@naver.com>
2023-07-03 23:35:23 +0900

    fix: 조회 쿼리 메서드 사용 시, 불필요한 Join 사용하는 이슈 개선

--- src/main/java/jparest/practice/subscription/repository/SubscriptionRepository.java
+++ src/main/java/jparest/practice/subscription/repository/SubscriptionRepository.java
@@ -3,14 +3,17 @@ package jparest.practice.subscription.repository;
 import jparest.practice.subscription.domain.Subscription;
 import jparest.practice.subscription.domain.SubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    Optional<Subscription> findByApplicantIdAndGroupIdAndStatus(UUID applicantId,
-                                                                Long groupId,
-                                                                SubscriptionStatus status);
-
+    @Query("SELECT s FROM Subscription s " +
+            "WHERE s.applicant.id = :applicantId AND s.group.id = :groupId AND status = :status")
+    Optional<Subscription> findByApplicantIdAndGroupIdAndStatus(@Param("applicantId") UUID applicantId,
+                                                                @Param("groupId") Long groupId,
+                                                                @Param("status") SubscriptionStatus status);
 }
